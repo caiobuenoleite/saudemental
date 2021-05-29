05dcf42c8fe2cb0bce9629a573a5856febc235b4
Caio Bueno Leite <leite.caiobueno@gmail.com>
2021-05-29 09:33:29 -0300

    final sexta

--- server.js
+++ server.js
@@ -1,26 +1,45 @@
+// import firebase from "firebase/app";
+// import "firebase/analytics";
+// import "firebase/auth";
+// import "firebase/firestore";
+
 const http = require('http')
 const fs = require('fs')
 const path = require('path')
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDbiqGKIAXBomz3rAIzAMhXSnHdXrG4TPQ",
+    authDomain: "plenisaude-1086c.firebaseapp.com",
+    projectId: "plenisaude-1086c",
+    storageBucket: "plenisaude-1086c.appspot.com",
+    messagingSenderId: "601884169648",
+    appId: "1:601884169648:web:e4475bb07fcc334605f275",
+    measurementId: "G-5B0EBD42BM"
+};
+
+// Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+
 http.createServer((req, res) => {
     if (req.url === '/') {
         fs.readFile(
-            path.join(__dirname, 'public', 'index.html'),
+            path.join(__dirname, 'public', 'indexpassado.html'),
             (err, content) => {
                 if (err) throw err
                 res.end(content)
             }
         )
     }
-    // if (req.url === '/protocolos') {
-    //     fs.readFile(
-    //         path.join(__dirname, 'public', 'index2.html'),
-    //         (err, content) => {
-    //             if (err) throw err
-    //             res.end(content)
-    //         }
-    //     )
-    // }
+    if (req.url === '/final') {
+        fs.readFile(
+            path.join(__dirname, 'public', 'final.html'),
+            (err, content) => {
+                if (err) throw err
+                res.end(content)
+            }
+        )
+    }
 
 
     // else if (req.url === '/segundoSB')
